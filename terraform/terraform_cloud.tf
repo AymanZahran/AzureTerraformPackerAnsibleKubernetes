8b95ebf9518edec005a6fd68099403abc604c4d3
Cloud User <sysadmin@Controller01.sj5us40lszqubjxgqtqberuikb.bx.internal.cloudapp.net>
2020-09-20 08:52:36 +0000

    now

---
+++ terraform/terraform_cloud.tf
@@ -0,0 +1,11 @@
+# Using a single workspace:
+# terraform {
+#   backend "remote" {
+#     hostname = "app.terraform.io"
+#     organization = "Ayman_Terraform_Final"
+
+#     workspaces {
+#       name = "Terraform_Final"
+#     }
+#   }
+# }
